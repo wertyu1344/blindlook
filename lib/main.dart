5201e27f9377711d3b5c1fe01149ff2fe889bd0d
omersaidakcin <omersaidakcin663@gmail.com>
2022-07-02 18:42:49 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:blindlook/widgets/bot_nav_bar/bot_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'controller/controller.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   Controller controller = Get.put(Controller());
