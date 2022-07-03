189b2a4e8cb741022c889977e80d758b47b6fe55
BunyaminAkdeniz <21060674@stu.omu.edu.tr>
2022-07-03 20:13:34 +0300

    int

--- lib/screens/request_screen/request_screen.dart
+++ lib/screens/request_screen/request_screen.dart
@@ -1,118 +1,20 @@
-import 'package:blindlook/constants/constants.dart';
-import 'package:blindlook/widgets/register_page_widgets/requst_type_widgets.dart';
+import 'package:blindlook/controller/controller.dart';
+import 'package:blindlook/screens/request_screen/request_screen_choose_type.dart';
+import 'package:blindlook/screens/request_screen/request_select_method.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class RequestScreen extends StatelessWidget {
   RequestScreen({Key? key}) : super(key: key);
-  Constants constants = Constants();
-  int itemCount = 4;
+  LoginClass controller = LoginClass();
+  List pages = [
+    RequestPageChooseRequest(itemCount: 4),
+    RequestSelectMethod(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Container(
-      padding: constants.pagePadding,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                height: size.height / 22,
-                width: size.width / 8,
-              ),
-              Text(
-                "Request",
-                style: constants.requestTextStyleTitle,
-              ),
-              Image.asset(
-                "assets/images/request_page_images/message-question.png",
-                height: size.height / 22,
-                width: size.width / 8,
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text("Choose your request type",
-              style: constants.requestTextStyleMedium),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              RequestWidgets(
-                  imagePath: "Request",
-                  padding: const EdgeInsets.only(right: 5)),
-              RequestWidgets(
-                  imagePath: "Feedback",
-                  padding: const EdgeInsets.only(right: 5)),
-              RequestWidgets(imagePath: "Complaint", padding: EdgeInsets.zero),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Past Request",
-            style: constants.requestTextStyleMedium,
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Expanded(
-            child: itemCount == 0
-                ? Center(
-                    child: Text(
-                      "No request yet",
-                      style: constants.requestTextStyleTitle,
-                    ),
-                  )
-                : ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: itemCount,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: ListTile(
-                            leading: Container(
-                                width: size.width / 7,
-                                height: size.width / 7,
-                                decoration: const BoxDecoration(
-                                  color: Color.fromARGB(243, 247, 247, 247),
-                                ),
-                                child: Center(
-                                  child: Image.asset(
-                                    "assets/images/request_page_images/heart.png",
-                                    width: 25,
-                                    height: 25,
-                                  ),
-                                )),
-                            title: Text(
-                              "Requst Tile $index",
-                              style: constants.requestTextStyleMedium,
-                            ),
-                            subtitle: Text(
-                              "JUN 10, 2022",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  color: constants.primaryColor),
-                            ),
-                          ));
-                    },
-                  ),
-          ),
-          const SizedBox(
-            height: 50,
-          )
-        ],
-      ),
-    );
+    return Obx(() => pages[controller.requestPageIndex.value]);
   }
 }
