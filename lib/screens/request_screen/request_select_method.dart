ad6ff2e7bd3d5b1155f736d8a595e7aff4dd2ac7
BunyaminAkdeniz <21060674@stu.omu.edu.tr>
2022-07-03 21:48:11 +0300

    int

--- lib/screens/request_screen/request_select_method.dart
+++ lib/screens/request_screen/request_select_method.dart
@@ -1,17 +1,43 @@
+import 'package:blindlook/constants/constants.dart';
+import 'package:blindlook/controller/controller.dart';
 import 'package:flutter/material.dart';
-
-import '../../widgets/register_page_widgets/register_pages_app_bar.dart';
+import 'package:get/get.dart';
 
 class RequestSelectMethod extends StatelessWidget {
-  const RequestSelectMethod({Key? key}) : super(key: key);
+  RequestSelectMethod({Key? key}) : super(key: key);
+  final Constants constants = Constants();
+  LoginClass controller = Get.find<LoginClass>();
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Container(
+      padding: constants.pagePadding,
       child: Column(
         children: [
-          RequestAppBar(size: size),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () => controller.requestPageIndex - 1,
+                icon: Image.asset(
+                  "assets/images/request_page_images/back.png",
+                  height: size.height / 22,
+                  width: size.width / 8,
+                ),
+              ),
+              Text(
+                "Request",
+                style: constants.requestTextStyleTitle,
+              ),
+              Image.asset(
+                "assets/images/request_page_images/message-question.png",
+                height: size.height / 22,
+                width: size.width / 8,
+              )
+            ],
+          ),
         ],
       ),
     );
